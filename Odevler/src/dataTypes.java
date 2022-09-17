0370cc252c3773503463f1b619d8a462e1226848
Emre Karatas <58826978+karatassemree@users.noreply.github.com>
2022-09-17 23:13:33 +0300

    Add files via upload

---
+++ Odevler/src/dataTypes.java
@@ -0,0 +1,16 @@
+public class dataTypes {
+    public static void main(String[] args) {
+        
+        //Java type güvenli bir dildir.
+        //int sayi = 12;
+        //sayi = "Ankara";
+
+        byte sayi = 127;
+        System.out.println(sayi);
+
+        char sayi2 = 'A';
+
+        boolean dogruMu = true;
+    }
+
+}
---
+++ Odevler/src/variables.java
@@ -0,0 +1,21 @@
+public class variables {
+    public static void main(String[] args) {
+
+        // variables Değişkenler
+        int ogrenciSayisi = 10;
+
+        System.out.println("Öğrenci Sayısı = " + ogrenciSayisi);
+        // System.out.println("Öğrenci Sayısı = 10");
+        // System.out.println("Öğrenci Sayısı = 9");
+
+        ogrenciSayisi = 13;
+        System.out.println("Öğrenci Sayısı = " + ogrenciSayisi);
+        // reusability - tekrar kullanabilirlik
+
+        ogrenciSayisi = 18;
+
+        String mesaj = "Öğrenci sayısı = ";
+        System.out.println(mesaj + ogrenciSayisi);
+    }
+
+}
