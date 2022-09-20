54838027232555724e5c70bedd08e455857a0dd7
Emre Karatas <fsm.emrekaratas@gmail.com>
2022-09-20 11:58:10 +0300

    Modified - FSM

--- Java Kamp 2022 - 1. Gün Odev 2/src/arraysDemo.java
+++ Java Kamp 2022 - 1. Gün Odev 2/src/arraysDemo.java
@@ -1,21 +1,32 @@
 public class arraysDemo {
     public static void main(String[] args) {
 
-        // String ogrenci1 = "Emre";
-        // String ogrenci2 = "Fevzi";
-        // String ogrenci3 = "Zeki";
+        String ogrenci1 = "Emre";
+        String ogrenci2 = "Fevzi";
+        String ogrenci3 = "Zeki";
 
-        // System.out.println(ogrenci1);
-        // System.out.println(ogrenci2);
-        // System.out.println(ogrenci3);
+        System.out.println(ogrenci1);
+        System.out.println(ogrenci2);
+        System.out.println(ogrenci3);
 
-        String[] ogrenciler = new String[3];
+        System.out.println("-----------------------------------");
+
+        String[] ogrenciler = new String[4];
         ogrenciler[0] = "Emre";
         ogrenciler[1] = "Zeki";
         ogrenciler[2] = "Fevzi";
+        ogrenciler[3] = "Salih";
+        // ogrenciler[4] = "Osman";
 
         for (int i = 0; i < ogrenciler.length; i++) {
             System.out.println("Öğrenci adı : " + ogrenciler[i]);
+        }
+
+        System.out.println("-----------------------------------");
+
+        // diğer yöntem
+        for (String ogrenci : ogrenciler) {
+            System.out.println("Öğrenci Adı : " + ogrenci);
         }
     }
 }
