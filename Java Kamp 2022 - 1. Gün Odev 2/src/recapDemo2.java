33f921e827a9875c33728b5404fe064f89b24dfd
Emre Karatas <fsm.emrekaratas@gmail.com>
2022-09-20 15:40:02 +0300

    Update - FSM

--- Java Kamp 2022 - 1. Gün Odev 2/src/recapDemo2.java
+++ Java Kamp 2022 - 1. Gün Odev 2/src/recapDemo2.java
@@ -1,5 +1,18 @@
 public class recapDemo2 {
     public static void main(String[] args) {
-        
+
+        double[] myList = { 1.2, 1.3, 4.3, 5.6, 4.6, 2.3, 2, 1, 3, 0 };
+        double total = 0;
+        double max = myList[0];
+
+        for (double number : myList) {
+            if (max < number) {
+                max = number;
+            }
+            total = total + number;
+            System.out.println(number);
+        }
+        System.out.println("Toplam = " + total);
+        System.out.println("En büyük = " + max);
     }
 }
